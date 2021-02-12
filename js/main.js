c8c4db04c9c4dc6e723800878129caf1c46059fc
vaaaarlos <kadu.cerq@gmail.com>
2021-02-12 16:21:52 -0300

    create food

--- js/main.js
+++ js/main.js
@@ -2,11 +2,18 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+let direction = "";
+let newFoodPos = () => Math.floor(Math.random() * 15 + 1) * box;
+
+let food = {
+  x: newFoodPos(),
+  y: newFoodPos()
+}
+
 snake[0] = {
   x: 7 * box,
   y: 7 * box
 };
-let direction = "";
 
 function createBG() {
   context.fillStyle = "lightgreen";
@@ -18,6 +25,11 @@ function createSnake() {
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
+}
+
+function createFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -32,30 +44,28 @@ function update (event) {
 function startGame() {
   createBG();
   createSnake();
+  createFood();
 
-  if (snake[0].x > 15 * box) snake[0].x = 0;
-  if (snake[0].x < 0 * box) snake[0].x = 15 * box;
-  if (snake[0].y > 15 * box) snake[0].y = 0;
-  if (snake[0].y < 0 * box) snake[0].y = 15 * box;
-
-  console.log("x = " + snake[0].x);
-  console.log("y = " + snake[0].y);
-
+  // snake movement
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
-
   if (direction == "left") snakeX -= box;
   else if (direction == "up") snakeY -= box;
   else if (direction == "right") snakeX += box;
   else if (direction == "down") snakeY += box;
-
   let newHead = {
     x: snakeX,
     y: snakeY
-  }
-
+  };
   snake.unshift(newHead);
-  snake.pop();
+  if (snakeX != food.x || snakeY != food.y) snake.pop();
+  else { food.x = newFoodPos(); food.y = newFoodPos(); }
+
+  // canvas border
+  if (snake[0].x > 15 * box) snake[0].x = 0;
+  if (snake[0].x < 0 * box) snake[0].x = 15 * box;
+  if (snake[0].y > 15 * box) snake[0].y = 0;
+  if (snake[0].y < 0 * box) snake[0].y = 15 * box;
 }
 
 let jogo = setInterval(startGame, 50);
