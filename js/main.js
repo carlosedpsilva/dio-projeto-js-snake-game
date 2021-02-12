4c49b92f9731bce9ac1a58e9cf686e72bfbe92c9
vaaaarlos <kadu.cerq@gmail.com>
2021-02-12 15:28:47 -0300

    snake controls

--- js/main.js
+++ js/main.js
@@ -6,23 +6,40 @@ snake[0] = {
   x: 8 * box,
   y: 8 * box
 };
-let direction = "right";
+let direction = "";
 
 function createBG() {
   context.fillStyle = "lightgreen";
-  context.fillRect(0, 0, 16 * box, 16 * box);
+  context.fillRect(0, 0, 16 * (box + 1), 16 * (box + 1));
 }
 
 function createSnake() {
   for(let i=0; i < snake.length; i++) {
     context.fillStyle = "green";
-    context.fillRect(snake[i].x, snake[i].y, box, box);
+    context.fillRect(snake[i].x, snake[i].y, box + 1, box + 1);
   }
+}
+
+document.addEventListener("keydown", update);
+
+function update (event) {
+  if (event.keyCode == 37 && direction != "right") direction = "left";
+  if (event.keyCode == 38 && direction != "down") direction = "up";
+  if (event.keyCode == 39 && direction != "left") direction = "right";
+  if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function startGame() {
   createBG();
   createSnake();
+
+  if (snake[0].x > 15 * box) snake[0].x = 0;
+  if (snake[0].x < 0 * box) snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box) snake[0].y = 0;
+  if (snake[0].y < 0 * box) snake[0].y = 16 * box;
+
+  console.log("x = " + snake[0].x);
+  console.log("y = " + snake[0].y);
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -41,4 +58,4 @@ function startGame() {
   snake.pop();
 }
 
-let jogo = setInterval(startGame, 1000);
+let jogo = setInterval(startGame, 50);
